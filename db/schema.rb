6b8454aecb4215b5d7f4cae66e1a09dd2048ba95
Arpan <Arpan.G>
2019-07-23 19:39:53 +0530

    Add association between posts and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_132629) do
+ActiveRecord::Schema.define(version: 2019_07_23_135928) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
